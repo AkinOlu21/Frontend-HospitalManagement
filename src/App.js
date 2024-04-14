14ca53f48c39277955c6cf942b1c084524717836
AkinOlu21 <kenneth.akinola@outlook.com>
2024-04-14 12:00:52 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import logo from './logo.png/Medimatrix.webp';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Appointment from './components/Appointment';
 import './App.css';
+import Home from './components/Home';
 
-function App() {
+
+const App = () => {
+  const [appointment, setAppointment] = useState ([]);
+  const [Doctors, setDoctors] = useState ([])
+
+  useEffect( () =>{
+fetch('')
+.then(data => setAppointment(data))
+
+  })
+
+  const addappointments = appointments => {
+    setAppointment([...appointment, appointments]);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+  <div className="App">   
+     <img src={logo} alt="MediMatrix Logo" width= '100' className='MediMatrixLogo' />
+      <h1>Welcome MediMatrix</h1>
+      
+  <nav>
+  <ul>
+    <li className='list-unstyled'>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About Us</Link></li>
+    <li><Link to="/contact">Contact Us</Link></li>
+    <li><Link to="/appointment">Appointment</Link></li>
+    </li>
+  </ul>
+</nav>
+
+
+ </div>
+
+  </Router>
+  
+    
   );
 }
 
